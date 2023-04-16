9003a43ee43221266d521c8fd28ad42ef44d3e67
spdharma777 <spdharma777@gmail.com>
2023-04-17 03:46:58 +0530

    REACT CRUD APP

---
+++ src/components/Employees.jsx
@@ -0,0 +1,19 @@
+const Employees =[
+    {
+        id:"1",
+        Name:"vijay",
+        Age:"48"
+    },
+     {
+        id:"2",
+        Name:"ajith",
+        Age:"51"
+    },
+     {
+        id:"3",
+        Name:"surya",
+        Age:"47"
+    }
+]
+
+ export default Employees;
